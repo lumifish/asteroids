971ef26c52e7aa5ef588b58c87c90ec307ea3d6c
lumifish <eni.lume@gmail.com>
2024-09-17 08:51:32 +0200

    added player

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pygame
-
-import constants
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    frames = 60
+    dt = 0
     game_running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while game_running:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(frames) / 1000
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
